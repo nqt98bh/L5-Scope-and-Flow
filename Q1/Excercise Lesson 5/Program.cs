bdeddd8cf9b4a88c6b90164952af13eeba23a69e
nqt98bh <nqthang.work@gmail.com>
2024-12-22 16:18:25 +0700

    L5- Scope and flow

---
+++ Q1/Excercise Lesson 5/Program.cs
@@ -0,0 +1,410 @@
+﻿
+using System;
+
+class Lesson5
+{
+   static void Part1()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+       
+            if (input1 == input2)
+            {
+                Console.WriteLine("two inputs is equal");
+            }
+        else
+        {
+            Console.WriteLine("two inputs is not equal");
+        }
+
+    }
+   
+    static void Part2()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 > input2)
+        {
+            Console.WriteLine($"{input1} is greater than {input2}");
+        }
+        else
+        {
+            Console.WriteLine($"{input2} is less than {input1}");
+        }
+    }
+
+    static void Part3()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 < input2)
+        {
+            Console.WriteLine($"{input1} is less than {input2}");
+        }
+        else
+        {
+            Console.WriteLine($"{input2} is greater than {input1}");
+        }
+    }
+
+    static void Part4()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 != input2)
+        {
+            Console.WriteLine($"{input1} is not equal {input2}");
+        }
+        else
+        {
+            Console.WriteLine($"{input2} is equal {input1}");
+        }
+    }
+
+    static void Part5()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 >= input2 )
+        {
+            Console.WriteLine($"{input1} is greater than or equal {input2}");
+        }
+      
+    }
+
+    static void Part6()
+    {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 <= input2)
+        {
+            Console.WriteLine($"{input1} is less than or equal {input2}");
+        }
+    }
+    static void Part7()
+    {
+        int number1 = 10;
+        int number2 = 3;
+        int question = number1 * number2;
+        int answered = int.Parse(Console.ReadLine());
+        if(answered == question)
+        {
+            Console.WriteLine("the answer is correct");
+        }
+        else
+        {
+            Console.WriteLine("the answer is incorrect");
+        }
+    }
+    static void Part8()
+    {
+        int oldEnoughToVote = 18;
+        int userAge = int.Parse(Console.ReadLine());
+        if (userAge >= oldEnoughToVote) 
+        {
+            Console.WriteLine("You can attend the vote");
+        }
+        else
+        {
+            Console.WriteLine("your age is not enough");
+        }
+    }
+    static void Part9()
+    {
+        int enemyHealth = int.Parse(Console.ReadLine());
+        if (enemyHealth == 100)
+        {
+            Console.WriteLine("Enemy has full health");
+        }
+        else if (enemyHealth  == 50)
+        {
+            Console.WriteLine("Enemy has half health");
+        }
+        else if(enemyHealth == 25)
+        {
+            Console.WriteLine("Enemy has quarter health");
+        }
+        else if (enemyHealth == 0)
+        {
+            Console.WriteLine("Enemy is dead");
+        }
+    }
+    static void Part10()
+    {
+        int userAge = int.Parse(Console.ReadLine());
+        if(userAge >= 18)
+        {
+            Console.WriteLine("User is eligible for a discount");
+        }
+        else
+        {
+            Console.WriteLine("User is not eligible for a discount");
+        }
+    }
+    static void Part11()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i == 5)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+    }
+    static void Part12()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i == 7)
+            {
+                break;
+            }
+            Console.WriteLine(i);
+        }
+    }
+    static void Part13()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+               
+                if (i == 10)
+                {
+                    continue;  
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+    static void Part14()
+    {
+        List<string> fruits  = new List<string>() { "apple", "orange", "pine" };
+        foreach (var fruit in fruits)
+        {
+            if(fruit == "apple")
+            {
+                Console.WriteLine("Aplle found");
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    static void Part15()
+    {
+        for(int i = 1;i <= 100; i++)
+        {
+            if(i % 10 ==0)
+            {
+                continue ;
+            }
+            Console.WriteLine(i);
+        }
+    }
+    static void Part16()
+    {
+        for(char letter = 'A'; letter <= 'G';  letter++)
+        {
+            if(letter == 'E')
+            {
+                continue;
+            }
+            Console.WriteLine(letter);  
+        }
+    }
+    static void Part17()
+    {
+        for (int num = 1; num <= 50; num++)
+        {
+                bool isPrime = true;
+
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break; 
+                    }
+                }
+                if (isPrime)
+                {
+                Console.WriteLine(num);
+                }
+        }
+    }
+    static void Part18()
+    {
+        for( int i = 1; i <=10; i++)
+        {
+            if (i == 3)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+
+        }
+    }
+    static void Part19()
+    {
+        int[] numbers = { 5, 12, 8, 20, 34, 56, 19, 20, 99 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if(numbers[i] == 20)
+            {
+                Console.WriteLine($" the index of 20 is {i}");
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    static void Part20()
+    {
+        List<string> fruits = new List<string> { "apple", "banana", "mango" };
+        foreach (var fruit in fruits)
+        {
+            if(fruit == "banana")
+            {
+                continue;
+            }
+            Console.WriteLine(fruit);
+        }
+    }
+    static void Part21()
+    {
+        float MonthlySale = int.Parse(Console.ReadLine());
+        float commission;
+        if (MonthlySale >= 10000)
+        {
+            commission = MonthlySale * 10 / 100;
+            Console.WriteLine($"the commission is {commission}");
+        }
+        else
+        {
+            commission = MonthlySale * 0.05f;
+            Console.WriteLine($"the commission is {commission:F2}");
+        }
+    }
+
+    static void Part22()
+    {
+        Random random = new Random();
+        int num1 = random.Next(0, 100);
+        int num2 = random.Next(0, 100);
+        Console.WriteLine($"Enter the sum of {num1} and {num2} : ");
+        int sum = num1 + num2;
+        int userAnswer = int.Parse(Console.ReadLine());
+        if (userAnswer == sum)
+        {
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect!, it is {sum}");
+        }
+
+    }
+    static void Part23()
+    {
+        int studentMark = int.Parse(Console.ReadLine());
+
+        int[] marks = new int[] { 85, 80, 75, 70, 65, 60, 55, 50, 0 };
+        string[] Grade = new string[] { "A+", "A", "B+", "B", "C+", "C", "D+", "D", "F" };
+
+        for (int i = 0; i < marks.Length; i++)
+        {
+            if (studentMark >= marks[i])
+            {
+                Console.WriteLine($"Grade :{Grade[i]}");
+                if (marks[i] >= 85)
+                {
+                    Console.WriteLine("Comment : Excellent !");
+                    break;
+                }
+                else if (marks[i] >= 80 && marks[i] < 85)
+                {
+                    Console.WriteLine("Comment : Well done.");
+                    break;
+                }
+                else if (marks[i] < 50)
+                {
+
+                    Console.WriteLine("Comment : See me.");
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    static void Part24()
+    {
+        Console.WriteLine("Enter sees_player (true/false):");
+        bool sees_player = bool.Parse(Console.ReadLine());
+
+        if (sees_player)
+        {
+            Console.WriteLine("Enter the distance:");
+            int dis_from_player = int.Parse(Console.ReadLine());
+            if (dis_from_player <= 1)
+            {
+                Console.WriteLine("Attack!");
+            }
+            else if (dis_from_player <= 4 && dis_from_player >= 2)
+            {
+                Console.WriteLine("the guard will advance");
+            }
+            else if (dis_from_player >= 5)
+            {
+                Console.WriteLine("Wait!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wait!");
+        }
+    }
+    public static void Main()
+    {
+        //Part1();
+        //Part2();
+        //Part3();
+        //Part4();
+        //Part5();
+        //Part6();
+        //Part7();
+        //Part8();
+        //Part9();
+        //Part10();
+        //Part11();
+        //Part12();
+        //Part13();
+        //Part14();
+        //Part15();
+        //Part16();
+        //Part17();
+        //Part18();
+        //Part19();
+        Part20();
+        Part21();
+        Part22();
+        Part23();
+        Part24();
+    }
+}
---
+++ Q2/Q2/Program.cs
@@ -0,0 +1,553 @@
+﻿using System;
+class Question2
+{
+    static void Q1()
+    {
+        int input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("One");
+                break;
+            case 2:
+                Console.WriteLine("Two");
+                break;
+            case 3:
+                Console.WriteLine("Three");
+                break;
+        }
+    }
+
+    static void Q2()
+    {
+        int character = int.Parse(Console.ReadLine());
+        switch (character)
+        {
+            case 1:
+                Console.WriteLine("Warrior");
+                break;
+            case 2:
+                Console.WriteLine("Mage");
+                break;
+            case 3:
+                Console.WriteLine("Archer");
+                break;
+            default:
+                Console.WriteLine("choose one of three characters");
+                break;
+        }
+    }
+    static void Q3()
+    {
+        int directions = int.Parse(Console.ReadLine());
+        switch (directions)
+        {
+            case 1:
+                Console.WriteLine("North");
+                break;
+            case 2:
+                Console.WriteLine("South");
+                break;
+            case 3:
+                Console.WriteLine("East");
+                break;
+            case 4:
+                Console.WriteLine("West");
+                break;
+            default:
+                Console.WriteLine("Choose 1 to 4");
+                break;
+        }
+    }
+    static void Q4()
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("invalid");
+                break;
+        }
+    }
+    static void Q5()
+    {
+        Console.WriteLine("1 for Sword, 2 for Bow, 3 for Staff, and 4 for Axe");
+        int character = int.Parse(Console.ReadLine());
+        switch (character)
+        {
+            case 1:
+                Console.WriteLine("Sword");
+                break;
+            case 2:
+                Console.WriteLine("Bow");
+                break;
+            case 3:
+                Console.WriteLine("Staff");
+                break;
+            case 4:
+                Console.WriteLine("Axe");
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+    static void Q6()
+    {
+        Console.WriteLine("1 for Fire, 2 for Water, 3 for Earth, 4 for Air, and 5 for Lightning");
+        int input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1: Console.WriteLine("Fire");
+                break;
+            case 2: Console.WriteLine("Water");
+                break;
+            case 3: Console.WriteLine("Earth");
+                break;
+            case 4: Console.WriteLine("Air");
+                break;
+            case 5: Console.WriteLine("Lightning");
+                break;
+            default: Console.WriteLine("Invalid");
+                break;
+        }
+
+    }
+    static void Q7()
+    {
+        Console.WriteLine("1 for Start Game, 2 for Options, 3 for Load Game, and 4 for Exit");
+        int input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("Start Game");
+                break;
+            case 2:
+                Console.WriteLine("Options");
+                break;
+            case 3:
+                Console.WriteLine("Load Game");
+                break;
+            case 4:
+                Console.WriteLine("Exit");
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+
+    static void Q8()
+    {
+        int input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("Talk");
+                break;
+            case 2:
+                Console.WriteLine("Trade");
+                break;
+            case 3:
+                Console.WriteLine("Quest");
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+    static void Q9()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" first approach!");
+                break;
+            case 2:
+                Console.WriteLine("second approach!");
+                break;
+            case 3:
+                Console.WriteLine("third approach!");
+                break;
+            case 4:
+                Console.WriteLine("fourth approach!");
+                break;
+            case 5:
+                Console.WriteLine("fifth approach!");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q10()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" Hello!");
+                break;
+            case 2:
+                Console.WriteLine("Sorry!");
+                break;
+            case 3:
+                Console.WriteLine("Go ahead!");
+                break;
+            case 4:
+                Console.WriteLine("Unfortunately!");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q11()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" Position");
+                break;
+            case 2:
+                Console.WriteLine("Weapon");
+                break;
+            case 3:
+                Console.WriteLine("Armor");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q12()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" Scene 1");
+                break;
+            case 2:
+                Console.WriteLine("Scene 2");
+                break;
+            case 3:
+                Console.WriteLine("Scene 3");
+                break;
+            case 4:
+                Console.WriteLine("Scene 4");
+                break;
+            case 5:
+                Console.WriteLine("Scene 5");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q13()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" Kill 5 wolves");
+                break;
+            case 2:
+                Console.WriteLine("Help farmer");
+                break;
+            case 3:
+                Console.WriteLine("Go to city center");
+                break;
+            case 4:
+                Console.WriteLine("Kill a Dragon");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q14()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(" Health Potion");
+                break;
+            case 2:
+                Console.WriteLine("Mana Potion");
+                break;
+            case 3:
+                Console.WriteLine("Stamina Potion");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q15()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Left");
+                break;
+            case 2:
+                Console.WriteLine("Right");
+                break;
+            case 3:
+                Console.WriteLine("Jump");
+                break;
+            case 4:
+                Console.WriteLine("Crouch");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q16()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Iron");
+                break;
+            case 2:
+                Console.WriteLine("Steel");
+                break;
+            case 3:
+                Console.WriteLine("Gold");
+                break;
+            case 4:
+                Console.WriteLine("Diamond");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q17()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Fire");
+                break;
+            case 2:
+                Console.WriteLine("Water");
+                break;
+            case 3:
+                Console.WriteLine("Earth");
+                break;
+            case 4:
+                Console.WriteLine("Air");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 5.");
+                break;
+        }
+    }
+    static void Q18()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Strength");
+                break;
+            case 2:
+                Console.WriteLine("Agility");
+                break;
+            case 3:
+                Console.WriteLine("Intelligence");
+                break;
+            case 4:
+                Console.WriteLine("Vitality");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 4.");
+                break;
+        }
+    }
+    static void Q19()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Solution A");
+                break;
+            case 2:
+                Console.WriteLine("Solution B");
+                break;
+            case 3:
+                Console.WriteLine("Solution C");
+                break;
+            case 4:
+                Console.WriteLine("Solution D");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 4.");
+                break;
+        }
+    }
+    static void Q20()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Herb");
+                break;
+            case 2:
+                Console.WriteLine("Mushroom");
+                break;
+            case 3:
+                Console.WriteLine("Flower");
+                break;
+            case 4:
+                Console.WriteLine("Solution D");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 4.");
+                break;
+        }
+    }
+    static void Q21()
+    { 
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Talk");
+                break;
+            case 2:
+                Console.WriteLine("Trade");
+                break;
+            case 3:
+                Console.WriteLine("Quest");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 3.");
+                break;
+        }
+    }
+    static void Q22()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Desert");
+                break;
+            case 2:
+                Console.WriteLine("Forest");
+                break;
+            case 3:
+                Console.WriteLine("Mountain");
+                break;
+            case 4:
+                Console.WriteLine("Castle");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 4.");
+                break;
+        }
+    }
+    static void Q23()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Talk to a wich");
+                break;
+            case 2:
+                Console.WriteLine("Go to castle");
+                break;
+            case 3:
+                Console.WriteLine("Kill 5 pigs");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 3.");
+                break;
+        }
+    }
+    static void Q24()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Start");
+                break;
+            case 2:
+                Console.WriteLine("Option");
+                break;
+            case 3:
+                Console.WriteLine("Load game");
+                break;
+            case 4:
+                Console.WriteLine("Exit");
+                break;
+            default:
+                Console.WriteLine("Invalid selection! Please choose a number between 1 and 4.");
+                break;
+        }
+    }
+    public static void Main()
+    {
+        //Q1();
+        //Q2();
+        //Q3();
+        //Q4();
+        //Q5();
+        Q6();
+        Q7();
+        Q8();
+        Q9();
+        Q10();
+        Q11();
+        Q12();
+        Q13();
+        Q14();
+        Q15();
+        Q17();
+        Q18();
+        Q19();
+        Q20();
+        Q21();
+        Q22();
+        Q23();
+        Q24();
+    }
+}
